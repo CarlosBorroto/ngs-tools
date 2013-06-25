14e4dfd394eca88b9147b8cd2f2adefb009b9059
Carlos Borroto <caros.borroto@gmail.com>
2013-06-25 17:14:19 -0400

    Added --barcode option to mid-split. Reverted back to global importing.

--- fourfivefour/tools.py
+++ fourfivefour/tools.py
@@ -3,6 +3,13 @@ import fileinput
 import warnings
 import collections
 
+# import python-levenshtein
+try:
+    import Levenshtein
+except ImportError:
+    sys.exit("""Error: Python Levenshtein is required by '454-tools'
+https://pypi.python.org/pypi/python-Levenshtein""")
+
 # import docopt
 try:
     from docopt import docopt
@@ -44,34 +51,32 @@ options:
 
 def mid_split(options):
     """
-usage: 454-tools mid-split [options] [--] <mid_file> [<input_file>...]
+usage: 454-tools mid-split [--barcode BARCODE]... [options] [--] <mid_file> [<input_file>...]
 
 options:
     -h --help                           Show this screen.
 
-    -q --fastq                          Input file or stdin is in FASTQ format. By default FASTA format is expected.
+    -b BARCODE --barcode=BARCODE        Barcode to use from the <mid_file>. Allow multiple appearances.
+                                        By default all barcodes in the <mid_file> are used.
+    -q --fastq                          Input file or stdin is in FASTQ format. By default FASTA format
+                                        is expected.
     -k --keep-barcode                   Do not trim the barcode.
-    -d INTEGER --max-distance=INTEGER   Max Levenshtein's distance when looking for mutated barcodes [default: 3].
+    -d INTEGER --max-distance=INTEGER   Max Levenshtein's distance when looking for mutated barcodes
+                                        [default: 3].
     -s INTEGER --barcode-size=INTEGER   Barcode size [default: 11].
     """
-
-    # import python-levenshtein
-    try:
-        import Levenshtein
-    except ImportError:
-        sys.exit("""Error: Python Levenshtein is required by '454-tools mid-split'
-    https://pypi.python.org/pypi/python-Levenshtein""")
 
     options_mid_split = docopt(mid_split.__doc__, argv=options)
     m = options_mid_split['<mid_file>']
     i = options_mid_split['<input_file>']
+    b = options_mid_split['--barcode']
     f = 'fastq' if options_mid_split['--fastq'] else 'fasta'
     k = options_mid_split['--keep-barcode']
     d = int(options_mid_split['--max-distance'])
     s = int(options_mid_split['--barcode-size'])
 
     try:
-        _split_reads(i, m, f, d, s, k)
+        _split_reads(i, m, b, f, d, s, k)
     except ValueError as e:
         print e
 
@@ -87,36 +92,36 @@ def _open_output_handle(output):
 
     return handle
 
-def _split_reads(input_files, mid_file, format="fasta", max_distance=3, barcode_size=11, keep_barcode=False):
+def _split_reads(input_files, barcode_file, barcode_list, format="fasta", max_distance=3, barcode_size=11, keep_barcode=False):
     """
     Given a fasta/fastq set of reads files and a file with the barcode index. Create one 
-    fasta/fastq file for each group based on the closest matching barcode.
+    fasta/fastq file for each barcode_name based on the closest matching barcode.
 
     Adapted from:
     https://gist.github.com/dgrtwo/3725741
     """
     try:
-        index = BarcodeIndex(mid_file, max_distance, barcode_size)
+        index = BarcodeIndex(barcode_file, barcode_list, max_distance, barcode_size)
     except ValueError:
         raise
  
     counts = collections.defaultdict(int)
  
     try:
-        outfs = dict([(g, open(g + "." + format, "w")) for g in index.groups + ["Unassigned"]])
+        outfs = dict([(g, open("{0}.ld.{1}.{2}".format(g, max_distance, format), "w")) for g in index.barcode_names + ["Unassigned"]])
         try:
             f = fileinput.input(input_files)
             try:
                 for r in SeqIO.parse(f, format):
-                    group = index.find_barcode(str(r.seq[:barcode_size]))
-                    if group != None:
-                        group = group
+                    barcode_name = index.find_barcode(str(r.seq[:barcode_size]))
+                    if barcode_name != None:
+                        barcode_name = barcode_name
                         if not keep_barcode:
                             r = r[barcode_size:] 
                     else:
-                        group = "Unassigned"
-                    SeqIO.write([r], outfs[group], format)
-                    counts[group] += 1
+                        barcode_name = "Unassigned"
+                    SeqIO.write([r], outfs[barcode_name], format)
+                    counts[barcode_name] += 1
 
                 for k, v in sorted(counts.items(), key=lambda t: t[0] == "Unassigned"):
                     print k, v
@@ -139,22 +144,24 @@ class BarcodeIndex:
     Adapted from:
     https://gist.github.com/dgrtwo/3725741
     """
-    def __init__(self, index_file, max_distance=3, barcode_size=11):
+    def __init__(self, barcode_file, barcode_list, max_distance=3, barcode_size=11):
         self.cache = {}
         self.barcode_size = barcode_size
         try:
-            with open(index_file) as inf:
+            with open(barcode_file) as inf:
                 for l in inf:
                     l = l.rstrip('\n')
                     if l.startswith("#") or l == "":
                         continue
-                    g, barcode = l.split("\t")
+                    barcode_name, barcode = l.split("\t")
                     if len(barcode) != self.barcode_size:
                         warnings.warn("Ignoring barcode '{0}' as is not of correct length.".format(barcode))
                         continue
-                    self.cache[barcode] = g
+                    if barcode_list and barcode_name not in barcode_list:
+                        continue
+                    self.cache[barcode] = barcode_name
             self.barcodes = self.cache.keys()
-            self.groups = self.cache.values()
+            self.barcode_names = self.cache.values()
         except IOError as e:
             print "Cannot open file '{0}'. Error: {1}".format(e.filename, e.strerror)
             raise ValueError("Please supply a valid <mid_file>.")
@@ -162,7 +169,7 @@ class BarcodeIndex:
  
     def find_barcode(self, barcode):
         """
-        match barcode and return the group it's supposed to be in. If
+        match barcode and return the barcode_name it's supposed to be in. If
         there is none within the Levenshtein distance given, or if there
         is a tie, return None
         """
@@ -178,7 +185,7 @@ class BarcodeIndex:
         if best > self.max_distance or distances.count(best) > 1:
             return None
         # otherwise, return the best one, after caching it for future use
-        ret = self.groups[distances.index(best)]
+        ret = self.barcode_names[distances.index(best)]
         self.cache[barcode] = ret
         return ret
 
